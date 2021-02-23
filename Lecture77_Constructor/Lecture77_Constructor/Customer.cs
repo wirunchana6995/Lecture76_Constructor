39aa89394c6dc9451dcbcd00d4b99e7ce6d1bb49
wirunchana <wirunchana.pair@gmail.com>
2021-02-23 23:49:18 +0700

    Add Lecture 77 Construct

---
+++ Lecture77_Constructor/Lecture77_Constructor/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture77_Constructor
+{
+    class Customer
+    {
+        
+        string firstName = "";
+        string lastName = "";
+
+        //constructor ถูกสร้างเมื่อ object ถูก new ขึ้นมา
+        public Customer()
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        public Customer(string firstName, string lastName) //parameter ชื่อเหมือนตัวแปร firstName, lastName ข้างบน
+        {
+            Console.WriteLine("Hello " + firstName + " "+lastName);
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+    }
+}
